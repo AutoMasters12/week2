932ae35e828c717bc0244fe17d302a09c710a871
suman <suman143learning@gmail.com>
2020-02-23 11:44:51 +0530

    change logic of test 3

--- III/src/test/java/tests/TestSuite4.java
+++ III/src/test/java/tests/TestSuite4.java
@@ -8,7 +8,10 @@ public class TestSuite4 {
 
 	@Test
 	public void test() {
-	System.out.println("test1");
+	System.out.println("test2");
+	System.out.println("test13");
+	System.out.println("test14");
+	System.out.println("test12");
 	}
 
 }
