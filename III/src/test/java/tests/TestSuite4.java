086e2c66f5f0cf5a4339a4278aeb989877a23808
suman <suman143learning@gmail.com>
2020-02-23 11:49:35 +0530

    review chnges updated

--- III/src/test/java/tests/TestSuite4.java
+++ III/src/test/java/tests/TestSuite4.java
@@ -5,12 +5,11 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestSuite4 {
-
+//sample test
 	@Test
 	public void test() {
 	System.out.println("test2");
 	System.out.println("test13");
-	System.out.println("test14");
 	System.out.println("test12");
 	}
 
